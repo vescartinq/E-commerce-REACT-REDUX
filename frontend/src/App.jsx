4e22c6a660eb0d2bf81ae0a3015c5a90c5e55b54
Starkin28 <victoreq00@gmail.com>
2021-01-11 17:52:15 +0100

    product screen component created and navigation between views started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,32 @@
 import React from 'react';
-import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './views/HomeScreen'
+import ProductScreen from './views/ProductScreen'
 
-import Product from './components/Product'
 
 
 function App() {
 
   return (
-    <div className="grid-container">
-        <header className="row">
-            <div>
-                <a className="brand" href="index.html">TriSoul</a>
-            </div>
-            <div>
-                <a href="cart.html">Cart</a>
-                <a href="signin.html">Sign In</a>
-            </div>
-        </header>
-        <main>
-        <div>
-          <div className="row center">
-            {data.products.map((product) => (
-              <Product key={product._id} product={product}/>
-            ))}
-          </div>
-        </div>
-      </main>
-        <footer className="row center">All rights reserved</footer>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+          <header className="row">
+              <div>
+                  <a className="brand" href="index.html">TriSoul</a>
+              </div>
+              <div>
+                  <a href="cart.html">Cart</a>
+                  <a href="signin.html">Sign In</a>
+              </div>
+          </header>
+          <main>
+            <Route path="/product/:id" component={ProductScreen}/>
+            <Route path="/" component={HomeScreen} exact/>
+         
+        </main>
+          <footer className="row center">All rights reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 

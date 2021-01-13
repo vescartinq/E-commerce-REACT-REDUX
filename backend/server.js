58acee4cd98ab2092a56cfc98538062eb9594319
Starkin28 <victoreq00@gmail.com>
2021-01-13 13:13:06 +0100

    created routes on backend to link data.js file to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './data.js';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
+const port = process.env.PORT || 5000;
+const dbUrl = process.env.DBURL || 'mongodb://localhost/trisoul';
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 app.get('/api/products/:id', (req, res) => {
   const product = data.products.find((element) => element._id === req.params.id);
@@ -14,12 +24,18 @@ app.get('/api/products/:id', (req, res) => {
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
+app.use('/api/users', userRouter);
 
 app.get('/', (req, res) => {
   res.send('Server working well and hard');
 });
 
-const port = process.env.PORT || 5000;
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });

4b6d7568d52e4bbccd3485b2207c45a2f43f8e2e
Starkin28 <victoreq00@gmail.com>
2021-01-14 08:46:24 +0100

    created backend API to authenticate users

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,14 @@ import express from 'express';
 import mongoose from 'mongoose';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const port = process.env.PORT || 5000;
 const dbUrl = process.env.DBURL || 'mongodb://localhost/trisoul';
 

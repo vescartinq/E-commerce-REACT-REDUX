c27d82b9bae6ef87552b2fe4cb98835b4c62c6c1
Starkin28 <victoreq00@gmail.com>
2021-01-22 11:25:18 +0100

    solving problems MongoDB on server.js

--- backend/server.js
+++ backend/server.js
@@ -14,40 +14,33 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 5000;
-const dbUrl = process.env.DBURL || 'mongodb://localhost/trisoul';
-
-
-mongoose.connect(dbUrl, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/trisoul', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
-
 app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
-
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
-
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.use(express.static(path.join(__dirname,'/frontend/build')));
-app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'/frontend/build/index.html')));
-
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
 // });
-
-
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Serve at http://localhost:${port}`);
 });
